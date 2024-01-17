5d07652aa078369e058e4408f582c072151e5e15
Sakib Al Hasan <bsse1209@iit.du.ac.bd>
2024-01-17 13:43:59 +0600

    fetch data use api complete

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,37 +6,34 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FormArray } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DataService } from '../service/data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersService } from '../service/users.service';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, CommonModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule, HttpClientModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
-export class FormComponent implements OnInit{
-  userData:any;
-  userForm:any;
-  constructor(private formBuilder: FormBuilder, private dataService:DataService){
-    this.userData = this.dataService.getData();
-    this.userForm = new FormGroup({
-      name : new FormControl(this.userData.name),
-      email : new FormControl(this.userData.email),
-      phnNumber: new FormControl(this.userData.phnNumber),
-      experiences: this.formBuilder.array(this.userData.experience)
-    })
-  
+export class FormComponent{
+  userForm: any;
+  usersData: any;
+
+  constructor(private formBuilder: FormBuilder,private userService: UsersService ) {
+    this.userService.getValue().subscribe((data) => {
+      this.usersData = data;
+      this.userForm = new FormGroup({
+        name: new FormControl(this.usersData[0].name),
+        email: new FormControl(this.usersData[0].email),
+        phnNumber: new FormControl(this.usersData[0].phnNumber),
+        experiences: this.formBuilder.array(this.usersData[0].experience)
+      });
+    });
   }
 
-  
-    ngOnInit(): void {
-      console.log("oninit")
-      // this.userData = this.dataService.getData();
-      // this.userForm.controls.name = this.userData[0].name;
-    }
-
     get experiences() {
-      return this.userForm.controls.experiences as FormArray;
+      return this.userForm?.controls.experiences as FormArray;
     }
   
     addExperience() {
--- src/app/service/users.service.ts
+++ src/app/service/users.service.ts
@@ -5,6 +5,19 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class UsersService {
-
+  url = "http://localhost:3000/userData"
+  userData:any;
   constructor(private http:HttpClient) { }
+ 
+  getData(){
+    this.http.get(this.url).subscribe((data)=>{
+      this.userData = data;
+      console.log("ser: ",data);
+    })
+    return  this.userData;
+  }
+
+  getValue(){
+    return this.http.get(this.url);
+  }
 }
